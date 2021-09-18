44c57418b71b90187324ae22a3ff02bfcfa63b13
abhitrivedi99 <abhitrivedi99@gmail.com>
2021-09-18 17:45:40 +0530

    Csv file read

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,21 @@ import fs from 'fs'
 
 const matches = fs.readFileSync('football.csv', {
     encoding:'utf-8'
+}).split('\n').map((row: string) =>{
+    return row.split(',')
 })
 
-console.log(matches)
+let manUnitedWins = 0
+
+const matchResult = {
+    HomeWin : 'H',
+    AwayWin : 'A',
+    DrawWin : 'D'
+}
+
+for(let match of matches) {
+    if(match[1]==='Man United' && match[5] === matchResult.HomeWin) manUnitedWins++
+    if(match[2]==='Man United' && match[5] === matchResult.AwayWin) manUnitedWins++
+}
+
+console.log(`Man united won ${manUnitedWins}`)
